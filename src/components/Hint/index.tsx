95181134cb9e62a9c21003ae6387d85e9b4cc27f
sevendaySmile <sevendaySmile@outlook.com>
2024-11-01 13:45:14 +0800

    feat: shadCN ui Hint encapsulation

---
+++ src/components/Hint/index.tsx
@@ -0,0 +1,26 @@
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
+
+const Hint = ({ label, children, side, align, sideOffset, alignOffset, classname }: HintProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={100} defaultOpen>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent side={side} align={align} sideOffset={sideOffset} alignOffset={alignOffset} className={classname}>
+          <p>{label}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  )
+}
+
+export interface HintProps {
+  label: string
+  children: React.ReactNode
+  side?: 'top' | 'bottom' | 'left' | 'right'
+  align?: 'start' | 'center' | 'end'
+  sideOffset?: number
+  alignOffset?: number
+  classname?: string
+}
+
+export default Hint
--- src/pages/Typing/components/ResultScreen/AuthorButton.tsx
+++ src/pages/Typing/components/ResultScreen/AuthorButton.tsx
@@ -1,21 +1,14 @@
 import kai from '@/assets/kai.jpg'
+import Hint from '@/components/Hint'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
 
 export const AuthorButton = () => {
   return (
-    <TooltipProvider delayDuration={100}>
-      <Tooltip defaultOpen>
-        <TooltipTrigger>
-          <Avatar className="h-8 w-8 shadow-lg" onClick={() => window.open('https://kaiyi.cool', '_blank')}>
-            <AvatarImage src={kai} alt="Kai Homepage" />
-            <AvatarFallback>Kai</AvatarFallback>
-          </Avatar>
-        </TooltipTrigger>
-        <TooltipContent className="cursor-pointer" onClick={() => window.open('https://kaiyi.cool', '_blank')}>
-          <p>点击了解作者和更多作品 ❤️</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <Hint label="点击了解作者和更多作品💕">
+      <Avatar className="h-8 w-8 shadow-lg" onClick={() => window.open('https://kaiyi.cool', '_blank')}>
+        <AvatarImage src={kai} alt="Kai Homepage" />
+        <AvatarFallback>Kai</AvatarFallback>
+      </Avatar>
+    </Hint>
   )
 }
